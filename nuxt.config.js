9f4d269c8e94b4458ef96045e0dd864465ecb8b6
bushra-nathanhr <112486616+bushra-nathanhr@users.noreply.github.com>
2022-10-26 10:17:01 +0400

    updates file structure

--- nuxt.config.js
+++ nuxt.config.js
@@ -31,8 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: "~plugins/chartjs", ssr: false },
-    '@/plugins/vuetify.js'
+    '@/plugins/vuetify.js',
+    { src: "@plugins/chartjs", ssr: false },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
